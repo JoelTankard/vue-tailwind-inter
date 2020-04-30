fb51e1ee77cfbae861ea89f08e9271e7df548e3a
Joel Tankard <vinju404@gmail.com>
2020-04-30 20:18:22 +1000

    inter fix

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app" class="text-inter">
+  <div id="app">
     <div id="nav" class="absolute p-4">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
